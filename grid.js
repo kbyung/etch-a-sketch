3ddf24b8f4455c8e4f900a843e90d4dd92de89ed
William Koh <wkoh@hmc.edu>
2023-10-31 23:00:26 -0700

    Refactored to make the grid squares resize dynamically. Added new grid button that allows user to clear board and choose the size.

--- grid.js
+++ grid.js
@@ -1,22 +1,38 @@
+const newGrid = document.querySelector('.newGrid')
 const grid = document.querySelector('.grid')
 
-
 function createGrid(size) {
+    
     for(let i = 0; i < size; i++) {
         const row = document.createElement('div')
-        grid.appendChild(row)
+        
 
         for (let j = 0; j < size; j++) {
             const cell = document.createElement('div');
             cell.classList.add('cell');
+            cell.style.height = `${960/size}px`
+            cell.style.width = `${960/size}px`
             row.appendChild(cell);
 
             cell.addEventListener("mouseover", function() {
-                cell.style.cssText = 'background-color: black'
+                cell.style.backgroundColor = 'black'
             })
         }
+        grid.appendChild(row)
     }
 }
+
+function removeAllChidNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild)
+    }
+}
+
+newGrid.addEventListener("click", function() {
+    let input = prompt("Enter the number of squares per side")
+    removeAllChidNodes(grid)
+    createGrid(input);
+})
 
 
 
